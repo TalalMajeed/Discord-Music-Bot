ed139ccbc89c41077e240c87901c44ac39f3e7d3
TalalMajeed <m.talal.majeed@gmail.com>
2024-02-07 11:31:22 +0500

    V.1.0.D

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
-from main import render
 
-
-print('Bot starting...')
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -14,6 +11,3 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command(name='hello')
 async def hello(ctx):
     await ctx.send('Hello!')
-    
-render()
-bot.run("ODk2MzM4MDQxODE0MjU3NzE0.GjOSi2.gdcPXklF1aoxdrzhuSHBIQ9jYNBTprc0tlfElM")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Create a Hello World Flask app
 from flask import Flask
+from bot import bot
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +10,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    bot.run(os.environ.get('TOKEN'))
     app.run(debug=True, port=80)
