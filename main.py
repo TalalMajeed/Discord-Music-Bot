3a536ed73e26954b57e3d9d15448072df02fa972
TalalMajeed <m.talal.majeed@gmail.com>
2024-02-07 11:21:28 +0500

    V.1.0.D

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
+#Create a Hello World Flask app
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return("<p style=\"color:rgb(88, 101, 242);font-family:\'Segoe UI\'\">Discord Bot is Online</p>")
+def hello_world():
+    return 'Hello, World!'
 
-def run():
-    app.run(port=80)
-
-def render():
-  t = Thread(target=run)
-  t.start()
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
